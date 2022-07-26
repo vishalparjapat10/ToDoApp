b09705383595b991c4d3d7a2a8121370e3e26e31
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-07-27 00:34:52 +0530

    :construction: submission logic done

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -7,8 +7,21 @@ export default class
         this.state = {
             tasks:[
               {id:1,task:"React"},
-            ]
+            ],
+            curTask:""
           };
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            curTask: e.target.value,
+        })
+    }
+    handleSubmit = () =>{
+        this.setState({
+            tasks:[...this.state.tasks,{id:this.state.tasks.length+1,task:this.state.curTask}],
+            curTask:""
+        })
     }
   render() {
     return (
@@ -17,15 +30,15 @@ export default class
             <h1>ToDo</h1>
         </div>
         <div>
-          <input type="text" />
-          <button>Submit</button>
+          <input type="text" value={this.state.curTask} onChange={this.handleChange}/>
+          <button onClick={this.handleSubmit}>Submit</button>
         </div>
         <div>
             {
                 this.state.tasks.map((taskObj) => (
                     <li key={taskObj.id}>
                         <h2>{taskObj.task}</h2>
-                        <button>Delete</button>
+                        <button onClick={() => {this.handleDeleteTask(taskObj.id)}}>Delete</button>
                     </li>
                 ))
 
